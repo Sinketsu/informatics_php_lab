29ec7198c24010d985dad5b250662c12b51514a6
Sinketsu <ssolloss@yandex.ru>
2018-05-02 20:56:21 +0300

    Testing auth by cookie

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test index</title>
+</head>
+<body>
+    Hello,
+    <?php
+        include 'utils/dbworker.php';
+
+        if (isset($_COOKIE['sess_id'])) {
+            $pdo = get_PDO();
+            $stmt = $pdo->prepare("SELECT session_key,user_id,expiring_time FROM sessions WHERE
+                                            session_key = :key");
+            $stmt->execute(['key' => $_COOKIE['sess_id']]);
+            $row = $stmt->fetch();
+            if (!$row)
+                print '%username%';
+
+            $time = (int)$row['expiring_time'];
+            if (time() > $time)
+                print '%username%';
+
+            $stmt = $pdo->prepare("SELECT username FROM users WHERE id = :id");
+            $stmt->execute(['id' => $row['user_id']]);
+            $row = $stmt->fetch();
+
+            print $row['username'];
+        } else
+            print '%username%';
+    ?>
+</body>
+</html>
+
