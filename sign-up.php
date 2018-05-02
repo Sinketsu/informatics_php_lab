5f61af6259c95dcfc47ba6e2c642b2febe62db0c
Sinketsu <ssolloss@yandex.ru>
2018-05-02 19:34:09 +0300

    Try to add sign-up functionality

--- sign-up.php
+++ sign-up.php
@@ -13,10 +13,27 @@
     $password = $_POST['password'];
 
     $stmt = $pdo->prepare('SELECT username FROM users WHERE username = :username');
-    $stmt->execute(['email' => $username]);
+    $stmt->execute(['username' => $username]);
     $row = $stmt->fetch();
     if ($row) {
-        echo 'exist!';
-    } else {
-        echo 'not exist!';
+        header("Location: /user_already_exist.html", true, 301);
+        exit();
     }
+
+    $stmt = $pdo->prepare("INSERT INTO php_db.users (username, email, password) 
+                                    VALUES (:username, :email, :password)");
+
+    $fp = fopen('/dev/urandom', 'r');
+    $randomBytes = fread($fp, 24);
+    fclose($fp);
+    $salt = base64_encode($randomBytes);
+    $hashed_password = '10000$' . $salt . '$' . hash_pbkdf2('sha384', $password, $salt, 10000);
+
+    $stmt->execute([
+        'username' => $username,
+        'email' => $email,
+        'password' => $hashed_password
+    ]);
+
+
+
