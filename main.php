355ff11d1e1b33d1973bb5abac510173e226cf4e
Sinketsu <ssolloss@yandex.ru>
2018-05-03 22:09:51 +0300

    add main page template

--- main.php
+++ main.php
@@ -31,5 +31,107 @@
         }
     ?>
 </nav>
+<div class="container">
+    <div class="row" style="height: 100px"></div>
+    <div class="row justify-content-center">
+        <h4 class="text-secondary">There are tasks for you. Please, help us...</h4>
+    </div>
+    <div class="row" style="height: 20px"></div>
+</div>
+<div class="container">
+    <div class="row">
+        <a class="card text-white card-link bg-success rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2 " href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-info rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-dark rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Pizzaburger</p>
+                <p class="card-text text-white-50">@Sinketsu</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-success rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-info rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-dark rounded-0  col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Pizzaburger</p>
+                <p class="card-text text-white-50">@Sinketsu</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
+            <div class="card-body">
+                <h5 class="card-title ">FORENSICS</h5>
+                <h1 class="card-title text-center">105</h1>
+                <p class="card-text">Password manager</p>
+            </div>
+        </a>
+    </div>
+
+
+
+
+</div>
+
 </body>
 </html>
