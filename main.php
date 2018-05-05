90aab57401f5b02163f599ca7e7493d8b180a094
Sinketsu <ssolloss@yandex.ru>
2018-05-05 13:27:48 +0300

    try get tasks from db

--- main.php
+++ main.php
@@ -9,6 +9,7 @@
 
     <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="css/main.css" type="text/css" rel="stylesheet">
+    <link href="css/colors.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 <nav class="d-flex flex-column flex-md-row align-items-center p-2 px-md-4 mb-3 bg-white border-bottom box-shadow">
@@ -22,6 +23,7 @@
         include_once 'utils/auth.php';
 
         $user_id = auth_get_user($_COOKIE);
+        $username = null;
         if (!is_null($user_id)) {
             $username = get_user_by_id($user_id)['username'];
 
@@ -42,6 +44,56 @@
 </div>
 <div class="container">
     <div class="row">
+        <?php
+            include_once 'utils/dbworker.php';
+
+            $pdo = get_PDO();
+
+            $tasks = null;
+            if (!is_null($username)) {
+                $stmt = $pdo->prepare("SELECT
+                                                  tasks.*,
+                                                  ISNULL(v_t.task_id) as solved
+                                                FROM
+                                                  tasks
+                                                  LEFT JOIN (
+                                                              SELECT task_id
+                                                              FROM
+                                                                solvings
+                                                              WHERE
+                                                                username = :username
+                                                            ) v_t
+                                                    ON v_t.task_id = tasks.id;");
+                $stmt->execute(['username' => $username]);
+
+                $tasks = $stmt->fetchAll();
+            } else {
+                $stmt = $pdo->prepare("SELECT *
+                                                FROM tasks;");
+                $stmt->execute();
+
+                $tasks = $stmt->fetchAll();
+            }
+
+            foreach ($tasks as $task) {
+                print "<a class=\"card text-white card-link bg-success rounded-0 col-6 m-0 col-sm-4 
+                        col-lg-3 col-xl-2\" href=\"task.php?id=$task[id]\">";
+                print "\t<div class=\"card-body\">";
+                print "\t\t<h5 class=\"card-title \">$task[category]</h5>";
+                print "\t\t<h1 class=\"card-title text-center\">$task[cost]</h1>";
+                print "\t\t<p class=\"card-text\">$task[name]</p>";
+                print "\t</div>";
+
+                if (isset($task['solved']) and $task['solved'] == 0) {
+                    print "<div class=\"overlay align-items-center\">";
+                    print "\t<h1 class=\"col\">SOLVED</h1>";
+                    print "</div>";
+                }
+
+                print "</a>";
+            }
+
+        ?>
         <a class="card text-white card-link bg-success rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
             <div class="card-body">
                 <h5 class="card-title ">FORENSICS</h5>
@@ -99,7 +151,7 @@
                 <p class="card-text">Password manager</p>
             </div>
         </a>
-        <a class="card text-white card-link bg-warning rounded-0  col-6 m-0 col-sm-4 col-lg-3 col-xl-2 solved" href="#">
+        <a class="card text-white card-link bg-warning rounded-0  col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
             <div class="card-body">
                 <h5 class="card-title ">FORENSICS</h5>
                 <h1 class="card-title text-center">105</h1>
@@ -110,7 +162,7 @@
                 <h1 class="col">SOLVED</h1>
             </div>
         </a>
-        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2 solved" href="#">
+        <a class="card text-white card-link bg-danger rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2" href="#">
             <div class="card-body">
                 <h5 class="card-title ">FORENSICS</h5>
                 <h1 class="card-title text-center">105</h1>
