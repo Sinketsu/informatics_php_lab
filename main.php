9895d60448af398fecf6cfc73ba7dc7611e6361f
Sinketsu <ssolloss@yandex.ru>
2018-05-05 13:40:10 +0300

    try get tasks from db

--- main.php
+++ main.php
@@ -75,8 +75,9 @@
 
                 $tasks = $stmt->fetchAll();
             }
-            print $colors['FORENSICS'];
+
             foreach ($tasks as $task) {
+                print $colors[$task['category']];
                 print "<a class=\"card text-white card-link " . $colors[$task['category']] . " rounded-0 col-6 m-0 col-sm-4 col-lg-3 col-xl-2\" href=\"task.php?id=$task[id]\">\n";
                 print "\t<div class=\"card-body\">\n";
                 print "\t\t<h5 class=\"card-title \">$task[category]</h5>\n";
