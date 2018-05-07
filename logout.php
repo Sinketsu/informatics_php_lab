6729692c960b46b132189c130f1034a8e3c6717b
Sinketsu <ssolloss@yandex.ru>
2018-05-07 16:32:19 +0300

    fix URL redirection on logout.php

--- logout.php
+++ logout.php
@@ -12,5 +12,17 @@
                                     WHERE session_key = :sess_id");
     $stmt->execute(['sess_id' => $_COOKIE['sess_id']]);
 
-    header("Location: $_GET[path]", true, 303);
+    $path = isset($_GET['path']) ? filter_var($_GET['path'], FILTER_SANITIZE_URL) : '/';
+    $url = parse_url($path);
+    if (isset($url['host'])) {
+        header("Location: /error/bad_request.html", true, 303);
+        exit();
+    }
+    $urls = ['/main.php', '/task.php', '/scoreboard.php'];
+    if (!in_array($url['path'], $urls)) {
+        header("Location: /error/bad_request.html", true, 303);
+        exit();
+    }
+
+    header("Location: $path", true, 303);
     exit();
