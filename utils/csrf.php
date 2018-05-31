4498c9793e8bc318624021a45d482c5d6cd720f1
Sinketsu <ssolloss@yandex.ru>
2018-05-31 13:08:19 +0300

    fix some bugs

--- utils/csrf.php
+++ utils/csrf.php
@@ -11,11 +11,19 @@
     function check_csrf() {
         if (!isset($_COOKIE['X-CSRF-Token']) or !isset($_POST['CSRF-token'])) {
             header("Location: /error/csrf.html", true, 303);
+
+            // unset CSRF cookie
+            setcookie('X-CSRF-Token', '', time() - 3600);
+
             exit();
         }
 
         if ($_COOKIE['X-CSRF-Token'] != $_POST['CSRF-token']) {
             header("Location: /error/csrf.html", true, 303);
+
+            // unset CSRF cookie
+            setcookie('X-CSRF-Token', '', time() - 3600);
+
             exit();
         }
     }
